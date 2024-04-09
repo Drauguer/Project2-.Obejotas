e8f6cbc1fee57dd45b0366c5a3da3d15748955c7
SalvaBernades <salvador.bernades@estudiantat.upc.edu>
2024-04-09 12:30:49 +0200

    Mapas hechos

---
+++ Project2/Project2/Output/Assets/Maps/Tiles/.xml/A2_32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="A2_32x32" tilewidth="32" tileheight="32" tilecount="192" columns="16">
+ <image source="../32x32/A2_32x32.png" width="512" height="384"/>
+</tileset>
---
+++ Project2/Project2/Output/Assets/Maps/Tiles/.xml/Props-01.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Props-01" tilewidth="32" tileheight="32" tilecount="28" columns="4">
+ <image source="../Props-01.png" width="128" height="224"/>
+</tileset>
